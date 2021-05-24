1e2c625f6e4a5a97f1ae43e1b213b552ca7fec50
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-05-24 16:13:29 +0300

    completed

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,44 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    NSDateFormatter *ex1 = [NSDateFormatter new];
+    if (monthNumber > 12 || monthNumber < 1) {
+        return nil;
+    }
+    return [[ex1 monthSymbols] objectAtIndex:(monthNumber-1)];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *ex2 = [NSDateFormatter new];
+    ex2.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    NSDate *dateFromString = [ex2 dateFromString:date];
+    if (date == nil) {
+        return 0;
+    }
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay fromDate: dateFromString];
+    return [components day];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *weekday = [NSDateFormatter new];
+    weekday.locale = [NSLocale localeWithLocaleIdentifier:@"ru_RU"];
+    [weekday setLocale:weekday.locale];
+    [weekday setDateFormat: @"EE"];
+    return [weekday stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitWeekOfYear fromDate: [NSDate date]];
+    NSUInteger todaysWeek = [components weekOfYear];
+    NSDateComponents *otherComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitWeekOfYear fromDate: date];
+    NSUInteger datesWeek = [otherComponents weekOfYear];
+    return todaysWeek == datesWeek;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,28 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *mutStr1 = [NSMutableString stringWithString:string1];
+    NSMutableString *mutStr2 = [NSMutableString stringWithString:string2];
+    
+    NSMutableString *result = [NSMutableString stringWithString:@""];
+    
+    while(mutStr1.length != 0 || mutStr2.length != 0) {
+        if (mutStr1.length == 0) {
+            [result appendString:mutStr2];
+            break;
+        } else if (mutStr2.length == 0) {
+            [result appendString:mutStr1];
+            break;
+        }
+        if ([[mutStr1 substringToIndex:1] UTF8String][0] <= [[mutStr2 substringToIndex:1] UTF8String][0]) {
+            [result appendString: [mutStr1 substringToIndex:1]];
+            mutStr1 = [[mutStr1 substringFromIndex:1] mutableCopy];
+        } else {
+            [result appendString: [mutStr2 substringToIndex:1]];
+            mutStr2 = [[mutStr2 substringFromIndex:1] mutableCopy];
+        }
+    }
+    return [result copy];
 }
 
 @end
